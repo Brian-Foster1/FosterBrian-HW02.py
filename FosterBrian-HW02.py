89f6e7efc2b224fdc595614030f215911e1bdd35
Brian Foster <bfoster9@uwyo.edu>
2024-10-29 18:13:20 -0600

    FosterBrian-HW02.py

---
+++ FosterBrian-HW02.py
@@ -0,0 +1,7 @@
+echo "# FosterBrian-HW02.py" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Brian-Foster1/FosterBrian-HW02.py.git
+git push -u origin main
